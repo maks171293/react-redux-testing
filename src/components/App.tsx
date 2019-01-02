e39b3e16a9f4990130b3fa4729b891bb514a2616
Christopher Beards <BeardsCM@gmail.com>
2019-01-02 11:26:18 -0500

    Initial installs and file structure

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import '../App.css';
+
+class App extends React.Component {
+  render() {
+    return <div className="App">Hello React!</div>;
+  }
+}
+
+export default App;
